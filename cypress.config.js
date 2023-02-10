a4608c3eb3f1794ec9e83160408ae646afe3fac7
Lucien Martin-Gaffé <lucien.martin-gaffe@mobile-devices.fr>
2023-02-10 15:57:53 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://api.integration.munic.io/services/coverage',
+    env: {
+      admin_token: 'fake-token',
+    },
+  },
+});
